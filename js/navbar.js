6e3a7449934b52dac97387f82148783c640fcd51
jacmill <jakub3020.jmy@gmail.com>
2021-05-09 15:14:56 +0200

    added map to trasy.html and heading for it, changed navbar.js

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,23 @@
+let path = window.location.pathname;
+let page = path.split("/").pop();
+const navLogo = document.querySelector(".nav__logo");
+const nav = document.querySelector(".nav");
+if (page === "index.html") {
+  navLogo.style.opacity = "0";
+}
 window.onscroll = () => {
-    const navLogo  = document.querySelector(".nav__logo");
-    const nav = document.querySelector(".nav");
-    if ( document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-      navLogo.style.opacity = "1";
-      nav.style.background = "#ffffff";
-    } else {
-      navLogo.style.opacity = "0";
-      nav.style.background = "transparent";
-    }
-  };
+  if (
+    document.body.scrollTop > 20 ||
+    (document.documentElement.scrollTop > 20 && page === "index.html")
+  ) {
+    navLogo.style.opacity = "1";
+  } else if (page === "index.html") {
+    navLogo.style.opacity = "0";
+  }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    nav.style.background = "#ffffff";
+  } else {
+    nav.style.background = "transparent";
+  }
+};
+console.log(page);
